0eda0667caa285a72c58908953a72e6ce4919c64
Sandra F <178869328+SandraF1@users.noreply.github.com>
2025-09-04 10:36:25 +1000

    After U5 concept check before U5 grammer parser

---
+++ frontend/src/pages/Learn/UnitSubsections/U5_1_Introduction_to_personal_pronouns.jsx
@@ -0,0 +1,24 @@
+// src/components/GreekPronounsIntro.jsx
+import React from "react";
+
+export default function GreekPronounsIntro() {
+  return (
+    <div className="unit-section">
+      <h2>Introduction to Personal Pronouns in Koine Greek</h2>
+      <p>
+        In English, we have short words that replace nouns or proper nouns. For
+        example, instead of saying,
+        <em> “My neighbour grows strawberries”</em>, we might say,
+        <em> “He grows strawberries.”</em> <strong>He</strong>,{" "}
+        <strong>she</strong>
+        and <strong>it</strong> are examples of{" "}
+        <strong>personal pronouns</strong>.
+      </p>
+      <p>
+        In <strong>Koine Greek</strong>, personal pronouns also exist, and they
+        have <strong>number</strong>, <strong>grammatical gender</strong>, and
+        <strong>case</strong>.
+      </p>
+    </div>
+  );
+}
